4eaaa81c47d75840d11d6b429dcb3586974d4960
abghifareihand <info.abghifareihan@gmail.com>
2024-03-08 13:50:11 +0700

    add login and register templete

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; Goyclinic 2024
+  </div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+Route::get('login', function () {
+    return view('pages.auth.login');
+});
+
+Route::get('register', function () {
+    return view('pages.auth.register');
 });
