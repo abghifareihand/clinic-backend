b63e7d0b87beb65c3bde470a69eaaf86391edf97
abghifareihand <info.abghifareihan@gmail.com>
2024-03-08 17:05:20 +0700

    update code

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -4,12 +4,12 @@
             <a href="index.html">GOY CLINIC</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">St</a>
+            <a href="index.html">GOY</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
             <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Users</span></a>
+                <a href="" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Users</span></a>
                 <ul class="dropdown-menu">
                     <li class=''>
                         <a class="nav-link" href="{{ route('users.index') }}">All User</a>
